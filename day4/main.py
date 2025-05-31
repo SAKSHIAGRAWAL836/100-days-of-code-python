661a71396a3123b3ce5750afa06ac79ca8737263
sakshi-agrawal <156072754+SAKSHIAGRAWAL836@users.noreply.github.com>
2025-05-31 11:46:38 +0530

    rock paper scissors code

---
+++ day4/main.py
@@ -0,0 +1,50 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_image = [rock, paper, scissors]
+
+player = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors"))
+if player >= 0 and player <= 2:
+    print(game_image[player])
+
+computer = random.randint(0,2)
+print("Computer chose:")
+print(game_image[computer])
+
+#logic
+if player >= 3 or player < 0:
+    print("You typed in wrong. You Lose!")
+elif computer == 0 and player == 2:
+    print("You lose!")
+elif player == 0 and computer == 2:
+    print("You Win!")
+elif computer > player:
+    print("you lose!")
+elif player > computer:
+    print("you win!")
+
